a16aa9a4d7d29aced1fe2a237162118508dab4e7
Lamborshea <nanbo0810@gmail.com>
2019-01-23 18:02:24 +0800

    chore: 修改全局 gitignore

---
+++ vilanda-express/config/env.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: 8002,
+  DB_TYPE: "mongodb",
+  DB_USERNAME: "",
+  DB_PASSWORD: "",
+  DB_ADDRESS: "121.196.223.8",
+  DB_PORT: "27017",
+  DATABASE: "mongodb://121.196.223.8:27017/vilanda"
+};
